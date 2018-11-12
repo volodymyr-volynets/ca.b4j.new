39991a32389bc321cbf1b4ae81b2a43717c8bbd8
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2018-11-11 19:02:59 -0500

    bug fixes

--- application/Data/SQLPatches/20181111_periods.sql
+++ application/Data/SQLPatches/20181111_periods.sql
@@ -1,4 +1,4 @@
 UPDATE b4_periods c
 SET
-	c.b4_period_new_registrations = (SELECT COUNT(*) FROM b4_register a WHERE a.b4_register_status_id = 10 AND a.b4_register_period = c.b4_period_id),
-	c.b4_period_confirmed_registrations = (SELECT COUNT(*) FROM b4_register a WHERE a.b4_register_status_id = 20 AND a.b4_register_period = c.b4_period_id);
+	c.b4_period_new_registrations = (SELECT COUNT(*) FROM b4_register a WHERE (a.b4_register_status_id = 10 OR a.b4_register_status_id = 20) AND a.b4_register_period_id = c.b4_period_id),
+	c.b4_period_confirmed_registrations = (SELECT COUNT(*) FROM b4_register b WHERE b.b4_register_status_id = 20 AND b.b4_register_period_id = c.b4_period_id);
