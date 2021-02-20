4976b7fa0a8163ce9330c66428c2d6f4ba4622c8
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2021-02-20 16:09:29 -0500

    fix

--- application/Controller/Index.php
+++ application/Controller/Index.php
@@ -6,11 +6,10 @@ class Index extends \Object\Controller {
 	public $title = 'Home';
 
 	public function actionIndex() {
-		\Layout::addMessage('Break for Jesus is back!', SUCCESS);
-		\Layout::addMessage("The organizing committee is pleased to announce that B4J 2021 will take place virtually, during March
-Break, every day from 11am-12:15pm. The online camp for Ukrainian Catholic children in gr. 3-8 from
-our eparchy will run on ZOOM, and will include prayers, teachings, music, arts and crafts, games and
-various fun activities – all together and in smaller groups. There is a limit of 80 screens, and you must
-register in advance on this site, between January 24 - February 21.", SUCCESS);
+		\Layout::addMessage('Break for Jesus 2021 is changing again!', SUCCESS);
+		\Layout::addMessage('Camp B4J will now run APRIL 12-16, daily 11 am-12:15 pm.', SUCCESS);
+		\Layout::addMessage("The online camp for Ukrainian Catholic children in gr. 3-8 from
+our eparchy will run on ZOOM, and will include prayers, teachings, music, arts and crafts, games and various fun activities – all together and in smaller groups. There is a limit of 80 screens.
+*REGISTRATION HAS BEEN EXTENDED TO MARCH 14.", SUCCESS);
 	}
 }
